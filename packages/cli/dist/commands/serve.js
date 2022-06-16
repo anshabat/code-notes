5dc85e3c0d4640b4c72ec356c62974960328a993
anshabat <andriy.shabat@ridewithvia.com>
2022-06-16 10:51:52 +0300

    used serve function from local-api repo inside cli repo

--- packages/cli/dist/commands/serve.js
+++ packages/cli/dist/commands/serve.js
@@ -1,7 +1,12 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.serveCommand = void 0;
+const path_1 = __importDefault(require("path"));
 const commander_1 = require("commander");
+const local_api_1 = require("local-api");
 //<...> - mandatory argument from user
 //[...] - optional argument form user
 exports.serveCommand = new commander_1.Command()
@@ -10,5 +15,6 @@ exports.serveCommand = new commander_1.Command()
     .option('-p, --port <number>', 'port to run server on', '4005')
     .action((filename = 'notebook.js', options) => {
     //logic after user run serve command
-    console.log(filename, options);
+    const dir = path_1.default.join(process.cwd(), path_1.default.dirname(filename));
+    (0, local_api_1.serve)(parseInt(options.port), path_1.default.basename(filename), dir);
 });
--- packages/cli/src/commands/serve.ts
+++ packages/cli/src/commands/serve.ts
@@ -1,4 +1,6 @@
+import path from 'path';
 import { Command } from 'commander';
+import { serve } from 'local-api';
 
 //<...> - mandatory argument from user
 //[...] - optional argument form user
@@ -6,7 +8,8 @@ export const serveCommand = new Command()
   .command('serve [filename]')
   .description('Open a fine for editing')
   .option('-p, --port <number>', 'port to run server on', '4005')
-  .action((filename = 'notebook.js', options) => {
+  .action((filename = 'notebook.js', options: { port: string }) => {
     //logic after user run serve command
-    console.log(filename, options);
+    const dir = path.join(process.cwd(), path.dirname(filename));
+    serve(parseInt(options.port), path.basename(filename), dir);
   });
